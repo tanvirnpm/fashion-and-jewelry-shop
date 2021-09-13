faa3a88b6aa6d4a737cdf97475431374755dd8e5
tanvirnpm <tanvirnpm@gmail.com>
2021-09-13 10:15:55 +0600

    all problem fix

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ const loadProducts = () => {
 };
 loadProducts();
 
+
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    // resolved image Instead of images
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -19,11 +21,16 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h4>Rating: ${product.rating.rate}/${product.rating.count}</h4>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to cart</button>
+      
+      <button onclick="productShow(${product.id})" type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">
+        Details
+      </button>
       `;
-    document.getElementById("all-products").appendChild(div);
+      // <button id="details-btn" class="btn btn-danger">Details</button></div>
+      document.getElementById("all-products").appendChild(div);
   }
 };
 let count = 0;
@@ -32,12 +39,15 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  // call for update total value
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  // get float value instead of integer because of price have fraction value.
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,12 +56,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  // fixd 2 value after .
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  // fixd 2 value after .
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -73,8 +85,30 @@ const updateTaxAndCharge = () => {
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// single product show
+const productShow = (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => productShowDetails(data));
+}
+// single product show details
+const productShowDetails = (product) => {
+    const {title, description, category, image, rating, price} = product;
+    getElementByIdName('product_title').innerText = title;
+    getElementByIdName('product_description').innerText = description;
+    getElementByIdName('product_image').src = image;
+    getElementByIdName('product_category').innerText = category;
+    getElementByIdName('product_price').innerText = price;
+    getElementByIdName('product_rating_rate').innerText = rating.rate;
+    getElementByIdName('product_rating_count').innerText = rating.count;
+};
+// get element by id
+const getElementByIdName = id =>{
+  return document.getElementById(id);
+}
+
